7c83ca754d0dfa61b0d258abe0b49f84ca21e001
GastonOrquera <gastonorquera29@gmail.com>
2024-01-23 19:41:05 -0300

    Pengov-Page

--- main.js
+++ main.js
@@ -462,7 +462,37 @@ function changeLanguage() {
     }
 
 
+    ScrollReveal().reveal('.scroll-reveal', {
+        origin: 'left',
+        distance: '40px',
+        easing: 'cubic-bezier(0.5,0,0,1.2)',
+        duration: 1000,
+        interval: 200
+    });
 
+    ScrollReveal().reveal('.scroll-reveal2', {
+        origin: 'top',
+        distance: '40px',
+        easing: 'cubic-bezier(0.5,0,0,1)',
+        duration: 1000,
+        interval: 200
+    });
+
+    ScrollReveal().reveal('.scroll-revealLeft', {
+        origin: 'left',
+        distance: '10px',
+        easing: 'cubic-bezier(0.5,0,0,1)',
+        duration: 1000,
+        interval: 200
+    });
+
+    ScrollReveal().reveal('.scroll-revealRight', {
+        origin: 'right',
+        distance: '10px',
+        easing: 'cubic-bezier(0.5,0,0,1)',
+        duration: 1000,
+        interval: 200
+    });
 
     const audio = document.querySelector("audio");
     const title = document.querySelector(".h1-js");
